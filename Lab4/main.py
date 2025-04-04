3ddc75bca2d47191f74a742fd42e2328de94aef0
Freddy-Ms <maciejskrzypa2705@gmail.com>
2025-04-04 11:55:09 +0200

    Clean Up

--- Lab4/main.py
+++ Lab4/main.py
@@ -9,18 +9,15 @@ def apply_threshold(image):
                                  cv2.THRESH_BINARY, 11, 2)
 
 def remove_artifacts(image):
-    kernel = np.ones((2,2), np.uint8)
+    kernel = np.ones((3,3), np.uint8)
     opened = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
     closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
     return closed
 
-def resize_image(image, max_size=800):
-    height, width = image.shape[:2]
-    if max(height, width) > max_size:
-        scale = max_size / max(height, width)
-        new_size = (int(width * scale), int(height * scale))
-        return cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
-    return image
+def show_and_save_image(image, window_name, file_name):
+    cv2.imshow(window_name, image)
+    cv2.imwrite(file_name, image)
+
 
 def process_image(image_path):
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -31,21 +28,14 @@ def process_image(image_path):
     
     contoured = apply_contouring(image)
     thresholded = apply_threshold(image)
-    final_image = remove_artifacts(image)
+    art_image = remove_artifacts(image)
     
-    contoured_resized = resize_image(contoured)
-    thresholded_resized = resize_image(thresholded)
-    final_image_resized = resize_image(final_image)
+    show_and_save_image(contoured, 'Contoured Image', 'contoured.png')
+    show_and_save_image(thresholded, 'Thresholded Image', 'thresholded.png')
+    show_and_save_image(art_image, 'Artifact Removed Image', 'artifact_removed.png')
     
-    cv2.imwrite('contoured.png', contoured)
-    cv2.imwrite('thresholded.png', thresholded)
-    cv2.imwrite('final_processed_image.png', final_image)
-    
-    cv2.imshow('Contoured Image', contoured_resized)
-    cv2.imshow('Thresholded Image', thresholded_resized)
-    cv2.imshow('Final Processed Image', final_image_resized)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
-process_image('image.JPG')  
+if __name__ == "__main__":
+    process_image('image.JPG')  
